6483e29210f083c350235e7ed4b8ecdae925038a
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2022-02-26 12:14:17 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pybullet as p
+import time
+import pybullet_data
+
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+
+planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+
+
+robot = p.loadURDF("./FourStickedBody/urdf/FourStickedBody.urdf", startPos, startOrientation)
+
+maxForce = 0
+mode = p.VELOCITY_CONTROL
+
+
+for i in range(10):
+    p.setJointMotorControl2(robot, i, controlMode=mode, force=maxForce)
+
+
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+
+
+cubePos, cubeOrn = p.getBasePositionAndOrientation(robot)
+
+
+print(cubePos,cubeOrn)
+
+
+p.disconnect()
